7a4e53eb70508171e4b56b3c7c53f23b9e62b6ba
Arnaud de Mouhy <arnaud.demouhy@akerbis.com>
2018-02-05 17:44:56 +0100

    Enhanced build script

--- build.sh
+++ build.sh
@@ -6,7 +6,12 @@ VERSION=4.6.0
 MINOR_VERSION=4.6
 MAJOR_VERSION=4
 
-docker build --no-cache -t dehy/adminer:latest .
-docker tag dehy/adminer:latest dehy/adminer:$VERSION
-docker tag dehy/adminer:latest dehy/adminer:$MINOR_VERSION
+docker build --no-cache --pull -t dehy/adminer:latest .
 docker tag dehy/adminer:latest dehy/adminer:$MAJOR_VERSION
+docker tag dehy/adminer:latest dehy/adminer:$MINOR_VERSION
+docker tag dehy/adminer:latest dehy/adminer:$VERSION
+
+docker push dehy/adminer:$MAJOR_VERSION
+docker push dehy/adminer:$MINOR_VERSION
+docker push dehy/adminer:$VERSION
+docker push dehy/adminer:latest
