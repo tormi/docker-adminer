91a3149ec05223e0e55e2b4fa5ac7f91de7f2e16
Arnaud de Mouhy <arnaud@admds.net>
2021-02-10 20:21:05 +0100

    php 8.0

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -5,15 +5,15 @@ set -eux
 apk --update add \
     s6 \
     nginx \
-    php7-fpm \
-    php7-pdo_dblib \
-    php7-pdo_mysql \
-    php7-pdo_pgsql \
-    php7-pdo_sqlite \
-    php7-pecl-mongodb \
-    php7-xml \
-    php7-json \
-    php7-session
+    php8-fpm \
+    php8-pdo_dblib \
+    php8-pdo_mysql \
+    php8-pdo_pgsql \
+    php8-pdo_sqlite \
+    php8-pecl-mongodb \
+    php8-xml \
+    php8-json \
+    php8-session
 
 # Fixing file upload rights
 chown -R nginx: /var/lib/nginx
@@ -23,9 +23,9 @@ sed -r -i -e 's/upload_max_filesize = [0-9]+M/upload_max_filesize = 2000M/g' \
           -e 's/post_max_size = [0-9]+M/post_max_size = 2000M/g' \
           -e 's/max_execution_time = [0-9]+/max_execution_time = 300/g' \
           -e 's/memory_limit = [0-9]+M/memory_limit = 1024M/g' \
-    /etc/php7/php.ini
+    /etc/php8/php.ini
 
 sed -r -i -e 's/;request_terminate_timeout = [0-9]+/request_terminate_timeout = 300/g' \
-    /etc/php7/php-fpm.d/www.conf
+    /etc/php8/php-fpm.d/www.conf
 
 find /etc/services.d -name run -exec chmod 755 {} \;
